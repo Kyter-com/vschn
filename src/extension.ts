c8070f09a86d4f33470f8b5dbaee1e6668c1ea7a
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-05-09 09:30:37 -0700

    feat: add multiple dropdowns for options

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vschn" is now active!');
+  console.log("vschn activated");
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand("vschn.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from vschn!");
+  const command = vscode.commands.registerCommand("vschn.start", async () => {
+    const storyChoice = await vscode.window.showQuickPick(["Hot"], {
+      placeHolder: "Story Type",
+      title: "Choose which type of story you want to view",
+    });
+
+    const languageChoice = await vscode.window.showQuickPick(
+      ["HTML", "TypeScript"],
+      {
+        placeHolder: "Language",
+        title: "Choose which language you want to view",
+      }
+    );
+
+    console.log("storyChoice", storyChoice);
+    console.log("languageChoice", languageChoice);
   });
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(command);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
