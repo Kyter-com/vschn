c7dfc1c03c52c5c87e537314c3d88308d6a8aed9
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-06-21 20:56:12 -0700

    feat: show quick pick for language select

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,11 @@ import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   let startCommand = vscode.commands.registerCommand("vschn.start", () => {
-    vscode.window.showInformationMessage("Started vschn");
+    // Ask the user which coding language they want to view the results in
+    let language = vscode.window.showQuickPick(["JavaScript", "TypeScript"], {
+      title: "Select a language",
+      placeHolder: "Select a language",
+    });
   });
 
   context.subscriptions.push(startCommand);
